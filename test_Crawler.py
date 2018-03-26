a47b2bbf23457cdddf163740dd0be9751f56ac39
Gui Dalonso <guilherme.castro@fatec.sp.gov.br>
2018-03-26 14:49:39 -0300

    Atualização do argumento de cache nos testes

--- test_Crawler.py
+++ test_Crawler.py
@@ -3,19 +3,19 @@ import pytest
 import Crawler
 
 def test_input():
-    Crawler.spider(['https://canaltech.com.br/', 'https://jovemnerd.com.br/'], 'google', 2)
+    Crawler.spider(['https://canaltech.com.br/', 'https://jovemnerd.com.br/'], 'google', 2, False)
     
 
 def teste_input_errors():
     with pytest.raises(IndexError):
         #parametros digitados sem forma de array
-        Crawler.spider(('https://canaltech.com.br/', 'https://jovemnerd.com.br/'), 'google', 2)
+        Crawler.spider(('https://canaltech.com.br/', 'https://jovemnerd.com.br/'), 'google', 2, False)
     with pytest.raises(AttributeError):
         #Urls incompletas
-        Crawler.spider(['https://canaltech.com.br/', 'jovemnerd.com.br/'], 'google', 2)
+        Crawler.spider(['https://canaltech.com.br/', 'jovemnerd.com.br/'], 'google', 2, False)
         #Parametros que não representam urls
-        Crawler.spider(('https://canaltech.com.br/, https://jovemnerd.com.br/'), 'google', 2)
+        Crawler.spider(('https://canaltech.com.br/, https://jovemnerd.com.br/'), 'google', 2, False)
         #Página completamente em flash não há como fazer busca
-        Crawler.spider(('https://mono-1.com/monoface/main.html'), 'google', 2)
+        Crawler.spider(('https://mono-1.com/monoface/main.html'), 'google', 2, False)
         #termo de pesquisa vazio é inválido
-        Crawler.spider(['https://canaltech.com.br/', 'https://jovemnerd.com.br/'], '  ', 2)
+        Crawler.spider(['https://canaltech.com.br/', 'https://jovemnerd.com.br/'], '  ', 2, False)
